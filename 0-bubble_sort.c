077b0787b516f583a4686bb6406de8eea4e82156
Hassan <h23798a@gmail.com>
2024-02-20 07:21:31 +0200

    another solution

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,47 +1,46 @@
 #include "sort.h"
-void swap(int *a, int *b);
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order
- *
- * @array:An array of integers to sort.
- * @size: The size of the array.
- *
- * Description: Print the array after each swap.
- */
-void bubble_sort(int *array, size_t size)
-{
-	size_t i, new_len, len;
-
-	if (array == NULL || size < 2)
-		return;
-
-	len = size;
-	while (len > 1)
-	{
-		for (i = 1; i <= len - 1; i++)
-		{
-			if (array[i - 1] > array[i])
-			{
-				swap(&array[i - 1], &array[i]);
-				print_array(array, size);
-			}
-			new_len = i;
-		}
-		len = new_len;
-	}
-}
-
-/**
- * swap - Swap two integers in an array.
+ * swap_nodes - Swap two integers in an array.
  * @a: The first integer to swap.
  * @b: The second integer to swap.
  */
-void swap(int *a, int *b)
+void swap_nodes(int *a, int *b)
 {
 	int temp;
 
 	temp = *a;
 	*a = *b;
 	*b = temp;
+}
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, len = size;
+	int bubbly = 0;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == 0)
+	{
+		bubbly = 1;
+		for (i = 1; i < len; i++)
+		{
+			if (array[i - 1] > array[i])
+			{
+				swap_nodes(&array[i - 1], &array[i]);
+				print_array(array, size);
+				bubbly = 0;
+			}
+		}
+		len--;
+	}
 }
